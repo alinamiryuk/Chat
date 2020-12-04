24266e71162763b9c232dee592288c68dbc27f6a
alinamiryuk <alinamiryuk@mail.ru>
2020-12-04 23:40:44 +0300

    routes, models, db

---
+++ backend/pusher.js
@@ -0,0 +1,9 @@
+import Pusher from 'pusher'
+
+export const pusher = new Pusher({
+  appId: '1118122',
+  key: '764889e5181e81c377a5',
+  secret: 'f246635297f6e757780b',
+  cluster: 'eu',
+  useTLS: true,
+})
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,47 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import { messagesRouter } from './routes/messages.js'
+import { pusher } from './pusher.js'
+
+dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
+const DB_KEY = process.env.DB_KEY
 
-mongoose 
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.use('/api/messages', messagesRouter)
 
-app.listen(port, () => console.log(`port ${port}`))
+app.listen(PORT, (err) => {
+  if (err) console.log(err)
+  console.log(`port ${PORT}`)
+
+  mongoose.connect(DB_KEY, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  const db = mongoose.connection
+
+  db.once('open', () => {
+    console.log('database connected')
+
+    const messagesCollection = db.collection('messages')
+    const changeStream = messagesCollection.watch()
+
+    changeStream.on('change', (change) => {
+      if (change.operationType === 'insert') {
+        const messageDetails = change.fullDocument
+        pusher.trigger('messages', 'inserted', {
+          sender: messageDetails.user,
+          text: messageDetails.text,
+        })
+      } else {
+        console.log('error pusher')
+      }
+    })
+  })
+})
